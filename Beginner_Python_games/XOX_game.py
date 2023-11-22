712d5b8987d12c49abb94821b3a095fb176e4c35
InnovateXplorer <95949006+0806Anil@users.noreply.github.com>
2023-11-22 11:18:29 +0530

    Update XOX_game.py

--- Beginner_Python_games/XOX_game.py
+++ Beginner_Python_games/XOX_game.py
@@ -1,1 +1,66 @@
+import random
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    # Check rows, columns, and diagonals for a win
+    for i in range(3):
+        if all(cell == player for cell in board[i]) or all(board[j][i] == player for j in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+def is_board_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+def player_move(board):
+    while True:
+        try:
+            row = int(input("Enter the row (0, 1, or 2): "))
+            col = int(input("Enter the column (0, 1, or 2): "))
+            if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
+                return row, col
+            else:
+                print("Invalid move. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+def computer_move(board):
+    available_moves = [(i, j) for i in range(3) for j in range(3) if board[i][j] == ' ']
+    return random.choice(available_moves)
+
+def play_tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    players = ['X', 'O']
+    current_player = random.choice(players)
+
+    while True:
+        print_board(board)
+
+        if current_player == 'X':
+            row, col = player_move(board)
+        else:
+            print("Computer is making a move...")
+            row, col = computer_move(board)
+
+        board[row][col] = current_player
+
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"{current_player}: You won against computer")
+            break
+        elif is_board_full(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == "__main__":
+    print("Welcome to Tic-Tac-Toe!")
+    play_tic_tac_toe()
 
